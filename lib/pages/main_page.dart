4a869ce7bd84b1e3fd01d92d884bd01ba624625c
Andy Radkevich <andy@yoola.com>
2022-04-06 17:15:12 +0300

    fix issue with wrong project structure

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:superheroes/blocs/main_bloc.dart';
+import 'package:superheroes/pages/superhero_page.dart';
 import 'package:superheroes/resources/superheroes_colors.dart';
 import 'package:superheroes/resources/superheroes_images.dart';
 import 'package:superheroes/widgets/action_button.dart';
 
-import '../widgets/favorites.dart';
 import '../widgets/info_with_button.dart';
-import '../widgets/loading_indicator.dart';
-import '../widgets/min_symbols.dart';
 import '../widgets/search_result.dart';
+import '../widgets/superhero_card.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -142,6 +141,109 @@ class MainPageStateWidget extends StatelessWidget {
             );
         }
       },
+    );
+  }
+}
+
+class LoadingIndicator extends StatelessWidget {
+  const LoadingIndicator({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Align(
+      alignment: Alignment.topCenter,
+      child: Padding(
+        padding: EdgeInsets.only(top: 110),
+        child: CircularProgressIndicator(
+          color: SuperheroesColors.blue,
+          strokeWidth: 4,
+        ),
+      ),
+    );
+  }
+}
+
+class MinSymbols extends StatelessWidget {
+  const MinSymbols({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Align(
+      alignment: Alignment.topCenter,
+      child: Padding(
+        padding: EdgeInsets.only(top: 110),
+        child: Text(
+          "Enter at least 3 symbols",
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 20,
+            color: SuperheroesColors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Favorites extends StatelessWidget {
+  const Favorites({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 90),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            "Your favorites",
+            style: TextStyle(
+              fontWeight: FontWeight.w800,
+              fontSize: 24,
+              color: SuperheroesColors.white,
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: SuperheroCard(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const SuperheroPage(name: "Batman"),
+                ),
+              );
+            },
+            name: "Batman",
+            realName: "Bruce Wayne",
+            imageUrl:
+            "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg",
+          ),
+        ),
+        const SizedBox(height: 8),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: SuperheroCard(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const SuperheroPage(name: "Ironman"),
+                ),
+              );
+            },
+            name: "Ironman",
+            realName: "Tony Stark",
+            imageUrl:
+            "https://www.superherodb.com/pictures2/portraits/10/100/85.jpg",
+          ),
+        )
+      ],
     );
   }
 }
